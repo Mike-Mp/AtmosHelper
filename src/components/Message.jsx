7145826b84e0033c2d48c5324c24c7a1baab5312
Mike-Mp <mikemp@protonmail.com>
2022-06-25 13:42:10 +0300

    changes

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,18 +4,21 @@ export default function Message({ type, content, setMessage, actionToDo }) {
   console.log(type, content);
   return (
     <div className={`message message--${type}`}>
-      <span>{type}: </span>
-      <span>{content}</span>
-      <img src={closeIcon} onClick={() => setMessage({show: false})} />
+      <div className={`message__top`}>
+        <h3>{type} </h3>
+      <img src={closeIcon} onClick={() => setMessage({ show: false })} />
       {type === "confirm" ? (
         <div>
           <span>delete?</span>
-          <button onClick={setMessage({show: false})}>No</button>
+          <button onClick={setMessage({ show: false })}>No</button>
           <button onClick={actionToDo}>Yes</button>
         </div>
       ) : (
         ""
       )}
+      </div>
+      <p>{content}</p>
+      
     </div>
   );
 }
